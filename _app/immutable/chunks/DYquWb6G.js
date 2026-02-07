c7bcd3cf4e6c2456a5b80b7dd2b350f09c07b835
Martin Nordlund <mrtnprssn@gmail.com>
2026-02-07 19:34:10 +0100

    Updates

---
+++ _app/immutable/chunks/DYquWb6G.js
@@ -0,0 +1,1 @@
+import{f as a}from"./DkNzlG5d.js";a();
---
+++ _app/immutable/nodes/2.DTpGd0nS.js
@@ -0,0 +1,1 @@
+import{a as o,f as a}from"../chunks/c9S5vykA.js";import"../chunks/DYquWb6G.js";import{n as r}from"../chunks/DkNzlG5d.js";var m=a('<h1>Welcome to SvelteKit</h1> <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>',1);function s(t){var e=m();r(2),o(t,e)}export{s as component};
