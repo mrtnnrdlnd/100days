da6f4261ce0371c2b32dda9a67c4c35a2b8ac329
Martin Nordlund <mrtnprssn@gmail.com>
2026-02-08 15:11:22 +0100

    Updates

--- internal/immutable/chunks/B7B9MWaR.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_dxq0pf?.base??"/100days",a=globalThis.__sveltekit_dxq0pf?.assets??s??"";export{a,s as b};
---
+++ internal/immutable/chunks/Bhx99ngh.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1f68hg5?.base??"/100days",a=globalThis.__sveltekit_1f68hg5?.assets??s??"";export{a,s as b};
